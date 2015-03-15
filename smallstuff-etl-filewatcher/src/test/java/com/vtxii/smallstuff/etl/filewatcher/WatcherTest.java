f73089fd874d062a3ff492ddba9015292fef2e96
vtxii <vtxii@users.noreply.github.com>
2015-03-15 07:33:53 -0400

    Added license info

--- smallstuff-etl-filewatcher/src/test/java/com/vtxii/smallstuff/etl/filewatcher/WatcherTest.java
+++ smallstuff-etl-filewatcher/src/test/java/com/vtxii/smallstuff/etl/filewatcher/WatcherTest.java
@@ -1,4 +1,22 @@
+/**
+* Copyright 2015 VTXii
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*/
+
 package com.vtxii.smallstuff.etl.filewatcher;
+
+import static org.junit.Assert.fail;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -62,7 +80,7 @@ public class WatcherTest {
 			watcher.setRunning(false);
 			thread.join();
 		} catch (InterruptedException | IOException e) {
-			e.printStackTrace();
+			fail("with exception: " + e);
 		}
 	}
 	
@@ -83,7 +101,7 @@ public class WatcherTest {
 	      }
 	      writer.close();
 	    } catch (IOException | InterruptedException e) {
-			e.printStackTrace();
+			fail("with exception: " + e);
 		}		
 	}
 }
